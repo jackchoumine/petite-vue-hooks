4024b1b0824196cc2c46ec71dc5de453ec13cb63
ZhouQiJun <zhouqijun4job@163.com>
2024-05-02 02:34:05 +0800

    fixed: 组件激活和失活也要处理函数监听

--- src/hooks/browser/useOn.ts
+++ src/hooks/browser/useOn.ts
@@ -5,7 +5,11 @@
  * @LastEditTime: 2024-02-01 23:54:03
  * @Description : 事件绑定 hook
  */
-import { onMounted, onUnmounted } from 'vue'
+// @ts-nocheck
+import { isEmptyStr,isFunction, isObject} from 'petite-utils';
+import { onActivated, onDeactivated, onMounted, onUnmounted } from 'vue';
+
+
 
 type Handler = (event: Event) => void
 
@@ -14,9 +18,20 @@ export function useOn(
   handler: Handler,
   target: HTMLElement | Document | Window | BroadcastChannel
 ) {
+
+  if (!isEmptyStr(eventName)||!isFunction(handler)||!isObject(target)) return
+
   onMounted(() => {
     target.addEventListener(eventName, handler)
   })
+  onActivated(() => {
+    target.addEventListener(eventName, handler)
+  })
+
+  onDeactivated(() => {
+    target.removeEventListener(eventName, handler)
+  })
+
   onUnmounted(() => {
     target.removeEventListener(eventName, handler)
   })
