2c9f264d99884cf453d97616875bd1e7498b80f6
ZhouQiJun <zhouqijun4job@163.com>
2024-07-16 21:57:30 +0800

    chore: auto import

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,47 @@
  * @Author      : ZhouQiJun
  * @Date        : 2024-02-01 22:46:28
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2024-02-02 00:13:47
+ * @LastEditTime: 2024-07-16 21:35:42
  * @Description : vite 配置文件
  */
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { URL, fileURLToPath } from 'node:url'
+import AutoImport from 'unplugin-auto-import/vite'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), dts({ include: './src' })],
+  plugins: [
+    vue(),
+    vueJsx(),
+    dts({ include: './src' }),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/, // .md
+      ],
+      imports: ['vue'], //, 'vue-router']
+      // resolvers: [ElementPlusResolver()],
+      // eslint报错解决
+      eslintrc: {
+        enabled: true, // Default `false`
+        filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
+        globalsPropValue: true, // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
       e: fileURLToPath(new URL('./examples', import.meta.url)),
     },
+  },
+  server: {
+    open: true,
   },
   build: {
     lib: {
