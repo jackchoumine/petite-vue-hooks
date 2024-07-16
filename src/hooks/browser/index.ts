fb64ca41c492fc23932c32c2981beee4c6369c91
ZhouQiJun <zhouqijun4job@163.com>
2024-07-16 22:01:38 +0800

    feat: add useDraggable

--- src/hooks/browser/index.ts
+++ src/hooks/browser/index.ts
@@ -2,11 +2,11 @@
  * @Author      : ZhouQiJun
  * @Date        : 2024-02-01 12:45:50
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2024-07-16 21:56:35
+ * @LastEditTime: 2024-07-16 22:00:44
  * @Description : 浏览器相关 hook
  */
 export * from './useOn'
 export * from './useLocalStorage'
 export * from './useSessionStorage'
 export * from './useHover'
-// export * from './useDraggable'
+export * from './useDraggable'
